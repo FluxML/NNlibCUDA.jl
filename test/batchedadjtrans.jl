602715d747a7078f380860fe1d250a346ec6968e
Peter <adgjl5645@hotmail.com>
2022-06-26 11:24:14 +0800

    Update test/batchedadjtrans.jl
    Co-authored-by: Brian Chen <ToucheSir@users.noreply.github.com>

--- test/batchedadjtrans.jl
+++ test/batchedadjtrans.jl
@@ -12,16 +12,6 @@ end
     bay = batched_adjoint(y)
     bty = batched_transpose(y)
 
-    rbax = reshape(bax, :)
-    rbtx = reshape(btx, :)
-    rbay = reshape(bay, :)
-    rbty = reshape(bty, :)
-
-    rbax2 = reshape(bax, (12, 2))
-    rbtx2 = reshape(btx, (12, 2))
-    rbay2 = reshape(bay, (12, 2))
-    rbty2 = reshape(bty, (12, 2))
-
     @test sprint(show, bax) == sprint(show, bay)
     @test sprint(show, btx) == sprint(show, bty)
 
@@ -32,27 +22,23 @@ end
     @test collect(bax) == collect(bay)
     @test Array(btx) == Array(bty)
     @test collect(btx) == collect(bty)
+    
+    for shape in (:, (12, 2))
+        rbax = reshape(bax, shape)
+        rbtx = reshape(btx, shape)
+        rbay = reshape(bay, shape)
+        rbty = reshape(bty, shape)
 
-    @test sprint(show, rbax) == sprint(show, rbay)
-    @test sprint(show, rbtx) == sprint(show, rbty)
-
-    @test print_array_strs(rbax) == print_array_strs(rbay)
-    @test print_array_strs(rbtx) == print_array_strs(rbty)
-
-    @test Array(rbax) == Array(rbay)
-    @test collect(rbax) == collect(rbay)
-    @test Array(rbtx) == Array(rbty)
-    @test collect(rbtx) == collect(rbty)
-
-    @test sprint(show, rbax2) == sprint(show, rbay2)
-    @test sprint(show, rbtx2) == sprint(show, rbty2)
-
-    @test print_array_strs(rbax2) == print_array_strs(rbay2)
-    @test print_array_strs(rbtx2) == print_array_strs(rbty2)
-
-    @test Array(rbax2) == Array(rbay2)
-    @test collect(rbax2) == collect(rbay2)
-    @test Array(rbtx2) == Array(rbty2)
-    @test collect(rbtx2) == collect(rbty2)
+        @test sprint(show, rbax) == sprint(show, rbay)
+        @test sprint(show, rbtx) == sprint(show, rbty)
+    
+        @test print_array_strs(rbax) == print_array_strs(rbay)
+        @test print_array_strs(rbtx) == print_array_strs(rbty)
+    
+        @test Array(rbax) == Array(rbay)
+        @test collect(rbax) == collect(rbay)
+        @test Array(rbtx) == Array(rbty)
+        @test collect(rbtx) == collect(rbty)
+    end
 
 end
